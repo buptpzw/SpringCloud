3691f0baf7fa5a3b45ee8d48f616410056cee490
zhiweipan@sohu-inc.com <zhiweipan@sohu-inc.com>
2017-07-27 14:09:48 +0800

    add filter

--- apigateway/src/main/java/com/sohu/ApigatewayApplication.java
+++ apigateway/src/main/java/com/sohu/ApigatewayApplication.java
@@ -1,14 +1,21 @@
 package com.sohu;
 
+import com.netflix.zuul.ZuulFilter;
+import com.sohu.filter.ApiAuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class ApigatewayApplication {
+	@Bean
+	public ZuulFilter apiAuthFilter(){
+		return new ApiAuthFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
