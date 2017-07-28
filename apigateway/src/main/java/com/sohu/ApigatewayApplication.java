9902f95a19fb37531bf42112596edd9e22834770
zhiweipan@sohu-inc.com <zhiweipan@sohu-inc.com>
2017-07-28 14:50:10 +0800

    list service in Eureka server

--- apigateway/src/main/java/com/sohu/ApigatewayApplication.java
+++ apigateway/src/main/java/com/sohu/ApigatewayApplication.java
@@ -1,9 +1,14 @@
 package com.sohu;
 
+//import com.netflix.discovery.DiscoveryClient;
 import com.netflix.zuul.ZuulFilter;
 import com.sohu.filter.ApiAuthFilter;
+import com.sohu.filter.ApiRateLimitFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +20,20 @@ public class ApigatewayApplication {
 	@Bean
 	public ZuulFilter apiAuthFilter(){
 		return new ApiAuthFilter();
+	}
+
+	@Bean
+	public ZuulFilter apiRateLimitFilter(){
+		return new ApiRateLimitFilter();
+	}
+
+	@Bean
+	CommandLineRunner runner(@Qualifier("discoveryClient")DiscoveryClient dc) {
+		return args -> {
+			dc.getInstances("service-hi")
+					.forEach(si -> System.out.println(String.format(
+							"Found %s %s:%s", si.getServiceId(), si.getHost(), si.getPort())));
+		};
 	}
 
 	public static void main(String[] args) {
